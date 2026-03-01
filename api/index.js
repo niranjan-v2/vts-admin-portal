c0a78f702b756dda47a11a6865b01e104b3cf068
Niranjan Vasudevan <niranjanv233@gmail.com>
2026-03-01 10:36:51 +0800

    create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const PORT = 3000
 
+const encodedPassword = encodeURIComponent("valluvantamilschool@123");
+const connectionString = `mongodb+srv://admin:${encodedPassword}@vts-admin.hoqeksz.mongodb.net/?appName=vts-admin`
+
+mongoose.connect(connectionString).then(() => {
+    console.log('Database is connected');
+});
 const app = express();
 
 app.listen(PORT, () => {
