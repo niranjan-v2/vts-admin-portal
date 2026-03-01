69cfe1b23c1fdcd8cb3b0ba174ed19a9354444d0
Niranjan Vasudevan <niranjanv233@gmail.com>
2026-03-01 08:41:29 +0800

    Create pages and routes for home and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
